4ade433e275c2ee51a897deead8d8ef5f58dda93
Sean <spiacentini@wellscoleman.com>
2024-06-28 08:30:20 -0400

    Website Start

---
+++ Portfolio Website/main.py
@@ -0,0 +1,7 @@
+from datetime import date
+from flask import Flask, abort, render_template, redirect,url_for,flash
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+Bootstrap5(app)
+
