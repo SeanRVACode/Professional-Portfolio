6b8e30015599b2d73a53c0ec3c27d91d43174e80
Sean <spiacentini@wellscoleman.com>
2024-06-28 11:04:13 -0400

    Formatting changes

--- Portfolio Website/main.py
+++ Portfolio Website/main.py
@@ -7,4 +7,15 @@ Bootstrap5(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    thumb_path = '.static/assets/img/portfolio/thumbnails/'
+    full_path = '.static/assets/img/portfolio/fullsize/'
+    return render_template('index.html',thumb_path=thumb_path,full_path=full_path)
+
+# Handles the redirect for thumbnail images so I don't have to type url_for a ton
+@app.route('/assets/img/portfolio/thumbnails/<img>')
+def thumbnails(img):
+    return redirect(f'/static/assets/portfolio/thumbnails/{img}')
+
+@app.route('/assets/img/portfolio/fullsize/<img>')
+def fullsize(img):
+    return redirect(f'/static/assets/portfolio/fullsize/img')
