63be4b7281449b51ee6b3e8c7415d9b56611b81c
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2024-07-08 11:08:14 -0400

    Progress

--- Tic Tac Toe/main.py
+++ Tic Tac Toe/main.py
@@ -1,20 +1,66 @@
 
-def draw_board():
-    size = 3
-    board = [[" " for _ in range(3)] for _ in range(3)]
-    return board
+def play_game():
+    '''Starts the game,'''
+    def board():
+        size = 3
+        board = [[" " for _ in range(3)] for _ in range(3)]
+        return board
 
 
-# Ask Player if they want to be X or O
-def player_selection():
-    valid_choice = False
-    while not valid_choice:
-        player1 = input("Player 1 do you want to be X or O?\n").upper()
-        if player1.upper() == "X" or player1.upper() == "O":
-            valid_choice = True
+    # Ask Player if they want to be X or O
+    def player_selection():
+        valid_choice = False
+        while not valid_choice:
+            player1 = input("Player 1 do you want to be X or O?\n").upper()
+            if player1.upper() == "X" or player1.upper() == "O":
+                valid_choice = True
+            else:
+                print(f"Sorry {player1} is not a valid selection.\n")
+            
+        if player1 == "X":
+            player2 = "O"
         else:
-            print(f"Sorry {player1} is not a valid selection.\n")
-    return player1
+            player2 = "X"
+        return player1,player2
 
 
+
+    def draw_board(board):
+        for _ in board:
+            print(_,sep="\n")
+    
+    def check_winner(board,players=('X','O')):
+        for player in players:
+            
+            # Check rows
+            for row in board:
+                if all(cell == player for cell in row):
+                    return player
+            
+            # Check columns
+            for col in zip(*board):
+                if all(cell == player for cell in col):
+                    return player
+            # Check diags
+            if all(cell == player for cell in board[::4]):
+                return player
+            if all(cell == player for cell in board[2:-1:2]):
+                return player
+                
+    
+    player1,player2 = player_selection()
+
+    
+    
+    
+        
+    # board = board()        
+    # a = board
+    # choice_row = input("Which row do you want to put your piece in? (1-3)\n")
+    # choice_column = input("Which colum ndo you want to put your piece in? (1-3)\n")
+    # board[int(choice_row)-1][int(choice_column)-1] = "X" 
+    # draw_board(board)
+    # draw_board(a)
+    
+play_game()
 
