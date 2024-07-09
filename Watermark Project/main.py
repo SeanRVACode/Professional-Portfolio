e9a2efff105d45e9c97f3edbde63bcbd0542be1b
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2024-07-09 09:05:59 -0400

    Start

--- Watermark Project/main.py
+++ Watermark Project/main.py
@@ -1,1 +1,24 @@
 import customtkinter
+from PIL import Image
+import os,sys
+
+# Set Dark Mode
+customtkinter.set_appearance_mod('dark')
+customtkinter.set_default_color_theme('dark-blue')
+
+# Destroy window on close
+def on_window_close():
+    print('Window Closed')
+    root.destroy()
+
+# Set up root
+root = customtkinter.CTk()
+root.title('Watermark Image')
+root.geometry('500x350')
+root.protocol('WM_DELETE_WINDOW',on_window_close)
+
+
+
+
+    
+    
