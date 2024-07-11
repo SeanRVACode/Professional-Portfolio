2a8849de7c6f4e62975af1eaf09371bd14261773
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2024-07-11 09:24:33 -0400

    Commenting and read me update.

---
+++ Typing Speed Test Project/README.md
@@ -0,0 +1,5 @@
+**WPM Typing Test Project**
+
+Project for my professional portfolio using customtkinter for the GUI.
+
+Pressing start will cause the test to begin. Typing the sentence 100% correctly will determine your WPM.
--- Typing Speed Test Project/main.py
+++ Typing Speed Test Project/main.py
@@ -12,18 +12,17 @@ import random
 class TypingTest:
     def __init__(self,root):
         self.root = root
+        # Configures the grid.
         for i in range(4):
             self.root.grid_rowconfigure(i,weight=1,minsize=150)
         for j in range(3):
             self.root.grid_columnconfigure(j,weight=1,minsize=266)
-        # self.create_grid()
         ctk.set_appearance_mode('dark')
         ctk.set_default_color_theme('dark-blue')
         windll.shcore.SetProcessDpiAwareness(2)
         self.root.title('Typing Speed Test')
         self.root.geometry('800x600')
         self.root.protocol('WM_DELETE_WINDOW',self.on_window_close)
-
         self.sentences = ["The quick brown fox jumps over the lazy dog.","To be or not to be, that is the question.",
         "I have a dream that one day this nation will rise up.","Four score and seven years ago our fathers brought forth on this continent.","It is a truth universally acknowledged that a single man in possession of a good fortune must be in want of a wife.","It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness."]
         self.wpm = 0
@@ -42,22 +41,26 @@ class TypingTest:
         
 
     def start_button(self):
-        '''Function that sets up the start button.'''
+        '''Function that sets up the start button. Start button
+        also acts as a reset function.'''
 
         self.start_button = ctk.CTkButton(master=self.root,text='Start Game',command=self.start_time)
         self.start_button.grid(row=3,column=1,padx=5,pady=5,sticky='')
     
     def sentence_label(self):
+        '''Sentence label to display what the user is supposed to type.'''
 
         self.sentence_l= CTkLabel(master=self.root,font=('arial',20),text='Press Start To Begin',anchor='center',wraplength=750)
         self.sentence_l.grid(row=1,column=0,padx=5,pady=5,sticky='',columnspan=3)
         
     
     def create_labels(self):
+        '''Creates the labels on the application.'''
         self.sentence_label()
         self.wpm_label()
     
     def create_entry(self):
+        '''Entry for user.'''
         self.var = ctk.StringVar()
         self.var.trace_add('write',self.check_input)
         self.textbox = CTkEntry(master=self.root,placeholder_text='Enter Text here.',width=300,textvariable=self.var)
@@ -82,15 +85,10 @@ class TypingTest:
         self.sentence_l.configure(text=self.chosen_sentence)
         self.textbox.configure(state='normal')
         self.textbox.focus_set()
-        # match = False
-        
-        # while not match:
-        #     print(self.var)
-        #     if self.var == self.chosen_sentence:
-        #         match = True
-            
+
         
     def check_input(self,*args):
+        '''Constantly checks to see if user input matches the sentence.'''
         current_input = self.var.get()
         elapsed_time = time.time() - self.start
         if elapsed_time > 0:
@@ -103,19 +101,14 @@ class TypingTest:
             wpm = self.calculate_wpm()
             self.label_wpm.configure(text=f'WPM: {wpm}')
     
-    def calculate_wpm(self):
+    def calculate_wpm(self) -> int:
+        '''Calculates WPM and returns it as an int.'''
         elapsed_time = self.end - self.start
         wpm = round(len(self.chosen_sentence)/(5*(elapsed_time/60)),2)
         return wpm
         
-        
-            
-        
-        
-            
-            
-    
     def end_time(self):
+        '''Gets end time.'''
         self.end = time.time()
             
         
