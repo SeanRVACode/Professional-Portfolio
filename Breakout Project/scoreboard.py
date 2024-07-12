74c917684b5ff0089cc99ece86a88d9931cc0407
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2024-07-12 11:37:31 -0400

    Start Scoreboard

---
+++ Breakout Project/scoreboard.py
@@ -0,0 +1,11 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color('white')
+        self.penup()
+        self.hideturtle()
+    
