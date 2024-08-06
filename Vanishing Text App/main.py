5c1aaf60574a5fcc733d3f23f182ee0593aa4b35
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2024-08-06 14:05:56 -0400

    Moving to class system

--- Vanishing Text App/main.py
+++ Vanishing Text App/main.py
@@ -1,22 +1,49 @@
 import customtkinter as ctk
+from customtkinter import CTkLabel,CTkEntry
 from ctypes import windll
 import os,sys
 import time
 
-def on_window_close():
-    '''Destroys Root'''
-    print('Window Closed.')
-    root.destroy()
 
-# Appearance 
-ctk.set_appearance_mode('dark')
-ctk.set_default_color_theme('dark-blue')
-windll.shcore.SetProcessDpiAwareness(2)
 
-# General Set up
-root = ctk.CTk()
-root.title('Type Vanish')
-root.geometry('800x600')
-root.protocol('WM_DELETE_WINDOW',on_window_close)
+
+def start_button():
+    '''Start button to start the game.'''
+    pass
+
+def create_buttons():
+    '''Creates all buttons'''
+
+
+
 timer = 0
 
+class VanishingApp:
+    def __init__(self,root):
+        self.root = root
+        # Appearance 
+        ctk.set_appearance_mode('dark')
+        ctk.set_default_color_theme('dark-blue')
+        windll.shcore.SetProcessDpiAwareness(2)
+        # General Set up
+        self.root.title('Type Vanish')
+        self.root.geometry('800x600')
+        self.root.protocol('WM_DELETE_WINDOW',self.on_window_close)
+        # TODO need to set up grid system.
+                
+    def on_window_close(self):
+        '''Destroys Root'''
+        print('Window Closed.')
+        self.root.destroy()
+        
+    def create_buttons(self):
+        pass
+    
+    def start_button(self):
+        pass
+    
+    def text_entry_box(self):
+        '''Text entry box for user to type in.'''
+        # TODO need to finish setting up font and get it placed correctly within grid system.
+        text_entry = CTkEntry(self.root,font=(''))
+        pass
