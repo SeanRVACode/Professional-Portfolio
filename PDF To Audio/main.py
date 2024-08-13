27192ba531ff694cd97353a2758161f008965194
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2024-08-13 12:54:18 -0400

    Set up

---
+++ PDF To Audio/main.py
@@ -0,0 +1,9 @@
+import pypdf
+import pyttsx3
+
+# May need to use API instead of pyttsx3 
+# This thought is coming from comments made by people on the course.
+
+class Main:
+    def __init__(self):
+        pass
