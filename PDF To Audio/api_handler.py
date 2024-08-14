1d0e994aa173b4ca3edaec4fb5c6f6964356dbec
Sean <spiacentini@wellscoleman.com>
2024-08-14 12:33:12 -0400

    API Test Success

---
+++ PDF To Audio/api_handler.py
@@ -0,0 +1,35 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BASE_URL = "https://api.voicerss.org/?"
+
+
+class Con:
+    def __init__(self):
+        self.api_key = os.getenv('api_key')
+        self.language = "en-in"
+        
+        
+    def text_to_speech(self,src):
+        url = f'{BASE_URL}'
+        
+        params = {
+            'key':self.api_key,
+            'src':src,
+            'hl':self.language,
+            'c':'MP3',
+        }
+        
+        response = requests.get(url,params=params)
+        
+        # Writes the response to an mp3 file for listening
+        with open('test.mp3','wb') as media:
+            media.write(response.content)
+            
+            
+if __name__ == "__main__":
+    x = Con()
+    x.text_to_speech('Hello World')
--- PDF To Audio/main.py
+++ PDF To Audio/main.py
@@ -1,9 +1,13 @@
 import pypdf
-import pyttsx3
+import requests
+from api_handler import Con
+
 
 # May need to use API instead of pyttsx3 
 # This thought is coming from comments made by people on the course.
+# http://www.voicerss.org/login.aspx Potential Free API for text to voice.
 
 class Main:
     def __init__(self):
         pass
+    
