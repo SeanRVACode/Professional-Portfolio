78e83893ed2ed1b235c576694b9f2e9cfda848f2
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2024-10-11 13:13:15 -0400

    Attempting to fix typing

--- Web Scraping Steam Data/main.py
+++ Web Scraping Steam Data/main.py
@@ -1,6 +1,7 @@
 from browser_processor import Scraper
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 url = 'https://pokemondb.net/pokedex/national'
@@ -15,15 +16,26 @@ soup = BeautifulSoup(response.text,'html.parser')
 
 pokemon = soup.find_all(name='span',class_='infocard-lg-data text-muted')
 
+types = []
 numbers = []
 names = []
 types = []
+type_test = []
 for mon in pokemon:
     numbers.append(mon.find('small').get_text(strip=True))
     names.append(mon.find(class_="ent-name").get_text(strip=True))
+    type_test.append(mon.find(class_=re.compile("itype ")))
+
+
+# type_links = types_tag.find_all('a')
+# types = [t.get_text(strip=True) for t in type_links]
+        
+print(type_test)
+    
 
 print(numbers)
 print(names)
+
 # for elem in pokemon.
 
 
