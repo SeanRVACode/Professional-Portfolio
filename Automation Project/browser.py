1bf754efd66ab7f2b1d1195f482f2d222d873128
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2024-10-17 13:59:48 -0400

    Working Version

--- Automation Project/browser.py
+++ Automation Project/browser.py
@@ -9,17 +9,28 @@ class Dino:
         self.screenWidth,self.screenHeight = pyautogui.size()
         
     def check_window(self,x,y):
-        # window = pyautogui.screenshot(region=(x,y,200,100))
+        # Attempts to locate the exact color in a specific region of the screen.
         try:
-            check = pyautogui.locateOnScreen('./Images/color_of_obstacle.jpg',region=(x,y,200,100))
+            check = pyautogui.locateOnScreen('./Images/color_of_obstacle.jpg',region=(x,y,400,100),grayscale=True)
         except:
             return None
         return check
+    
+    def check_window_night(self,x,y):
+        try: 
+            check_white = pyautogui.locateOnScreen('./Images/white_color_of_obstacle.jpg',region=(x,y,400,100),grayscale=True)
+        except:
+            return None
+        return check_white
     def Auto_jump(self):
         while True:
             check = self.check_window(x=471,y=984)
             if check is not None:
                 self.jump()
+            elif check is None:
+                check_night = self.check_window_night(x=471,y=984)
+                if check_night is not None:
+                    self.jump()
     
     def jump(self):
         pyautogui.press('space')
