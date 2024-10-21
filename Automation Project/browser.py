44c73dfc7b8c5cdf4c1887f4b97abd73c0e52d43
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2024-10-21 07:42:26 -0400

    Changes to try and grab all needed pixels

--- Automation Project/browser.py
+++ Automation Project/browser.py
@@ -1,11 +1,13 @@
 import pyautogui
 import pyscreeze
+from PIL import ImageGrab
+import time
 
 
 
 class Dino:
     def __init__(self):
-        
+        pyautogui.FAILSAFE = False
         self.screenWidth,self.screenHeight = pyautogui.size()
         
     def check_window(self,x,y):
@@ -17,20 +19,35 @@ class Dino:
         return check
     def check_pixel(self,x,y):
         # TODO need to figure out if its easier to just check pixels
-        while True:
+        
             # im = pyautogui.screenshot(region=(x,y,400,100))
-            
+        while True:
             pix = pyautogui.pixelMatchesColor(x,y,(83,83,83))
-            print(pix)
-            return pix
+            pix_air = pyautogui.pixelMatchesColor(x,1011,(83,83,83))
+            pix_mid = pyautogui.pixelMatchesColor(x,1152,(83,83,83))
+            pix_wh = pyautogui.pixelMatchesColor(x,y,(172,172,172))
+            pix_wh_mid = pyautogui.pixelMatchesColor(x-10,1152,(172,172,172))
+            pix_wh_air = pyautogui.pixelMatchesColor(x,1011,(172,172,172))
+            if pix or pix_air or pix_mid or pix_wh or pix_wh_mid or pix_wh_air:
+                self.jump()
+                print(f'Pix: {pix} pix_air: {pix_air} pix_mid: {pix_mid}')
+                
+                # im = pyautogui.screenshot(region=(x-200,y-200,200,200))
+                # im.save('test_fail.png')
+            # time.sleep(.01)
+
     def get_cursor(self):
         '''Exists solely so I can check where my cursor is.'''
         while True:
             cursor = pyautogui.position()
-            im = pyautogui.screenshot(region=(500,994,400,200))
+            print(cursor)
+            im = pyautogui.screenshot(region=(625,1178,400,200))
             im.save('test.png')
             # pix = im.getpixel(cursor)
             print(cursor)
+            # pyautogui.moveTo(725,1100)
+            pix = pyautogui.pixel(cursor.x,cursor.y)
+            print(pix)
             # print(pix)
     # def check_window_night(self,x,y):
     #     try: 
@@ -45,6 +62,7 @@ class Dino:
             check = self.check_pixel(x=701,y=1100)
             # check_bird = self.check_pixel(x=801,y=897)
             if check:
+                print(check)
                 self.jump()
             # if check_bird:
             #     self.jump()
@@ -57,7 +75,7 @@ class Dino:
             #         self.jump()
     
     def jump(self):
-        pyautogui.press('space')
+        pyautogui.press('up')
             
             
             
--- Automation Project/main.py
+++ Automation Project/main.py
@@ -6,9 +6,12 @@ from browser import Dino
 def main():
     dino = Dino()
     # dino.get_cursor()
-    dino.Auto_jump()
+    # dino.Auto_jump()
+    # dino.check_pixel(x=625,y=1178)
+    # dino.check_pixel(x=725,y=1210)
+    dino.check_pixel(x=825,y=1164)
     
     
-    
+      
 if __name__ == '__main__':
     main()
