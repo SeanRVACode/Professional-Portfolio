f2a2317cbf19bb0736489d99c7a92e5ee0725f39
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2024-10-22 11:27:57 -0400

    Breaking into classes and functions for main

--- Invaders/main.py
+++ Invaders/main.py
@@ -5,56 +5,65 @@ import pygame
 WIDTH = 720
 HEIGHT = 1000
 
-def main():
-    pygame.init()
-    pygame.display.set_caption('Invaderrrsss')
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
-    running = True
-    clock = pygame.time.Clock()
-    ship = Ship(315,850,10,HEIGHT,WIDTH)
-    enemies = []
-    
-    
-    # # Create Enemies
-    xe = 30
-    ye = 30
-    for row in range(5):
-        for col in range(9):
-            ene = Enemy(5,HEIGHT,WIDTH,10,xe,ye)
-            enemies.append(ene)
-            xe += 80
-        xe = 30
-        ye += 40
-    
-    # End the game if the user presses 'X'
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                
-        # Fill the screen
-        screen.fill('black')
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('Invaderrrsss')
+        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        self.running = True
+        self.clock = pygame.time.Clock()
+        self.enemies = []
+        self.main()
+        
+    def main(self):
 
-        for enemy in enemies:
-            screen.blit(enemy.graphic,enemy.pos)
+        ship = Ship(315,850,10,HEIGHT,WIDTH)
+
         
-        # RENDER GAME HERE
-        # screen.blit(ship.graphic,(ship.position*5,0))
-        # Get Player Input
-        keys = pygame.key.get_pressed()
+
+        # # Create Enemies
+        self.enemies_setup(rows=4,cols=8)
         
-        # Ship Movement
-        if keys[pygame.K_LEFT]:
-            ship.move(left=True)
-        if keys[pygame.K_RIGHT]:
-            ship.move(right=True)
-        screen.blit(ship.graphic,(ship.pos))
-        
-        
-        # flip the display to put your work on screen
-        pygame.display.flip()
-        
-        clock.tick(60) # Limits the fps 60
-        
+        # End the game if the user presses 'X'
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    
+            # Fill the screen
+            self.screen.fill('black')
+
+            for enemy in self.enemies:
+                self.screen.blit(enemy.graphic,enemy.pos)
+            
+            # RENDER GAME HERE
+            # screen.blit(ship.graphic,(ship.position*5,0))
+            # Get Player Input
+            keys = pygame.key.get_pressed()
+            
+            # Ship Movement
+            if keys[pygame.K_LEFT]:
+                ship.move(left=True)
+            if keys[pygame.K_RIGHT]:
+                ship.move(right=True)
+            self.screen.blit(ship.graphic,(ship.pos))
+            
+            
+            # flip the display to put your work on screen
+            pygame.display.flip()
+            
+            self.clock.tick(60) # Limits the fps 60
+    def enemies_setup(self,rows,cols):
+        xe = 30
+        ye = 30
+        for row in range(rows+1):
+            for col in range(cols+1):
+                ene = Enemy(5,HEIGHT,WIDTH,10,xe,ye)
+                self.enemies.append(ene)
+                xe += 80
+            xe = 30
+            ye += 40
+                    
+            
 if __name__=='__main__':
-    main()
+    Game()
