728d7e450f306c4792f8b5111865da861b83aed8
Sean <seanwpgames@gmail.com>
2025-01-13 15:17:47 -0500

    Add initial GameOver class for game over screen setup

---
+++ Invaders/game_over.py
@@ -0,0 +1,14 @@
+import pygame.freetype
+from pygame.sprite import Sprite
+from pygame.rect import Rect
+
+
+class GameOver:
+    def __init__(self,text,font_size,text_rgb,bg_rgb,screen,screen_width,screen_heigh,score,high_score):
+        """Creates the game over screen. It also has the restart option."""
+        # TODO Finish setting up Game Over Screen
+        
+        pass
+    
+    
+
