525464ef4d8f94286ed6f86df36f1b78c1452e0c
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2025-02-20 15:16:55 -0500

    Implement brewery lookup feature with Flask and Bootstrap5; add templates for display

--- API Website Integration/main.py
+++ API Website Integration/main.py
@@ -1,5 +1,25 @@
-from flask import Flask,redirect,render_template
-from flask_bootstrap import Bootstrap
+from flask import Flask,redirect,render_template,jsonify
+from flask_bootstrap import Bootstrap5
+import requests
+import json
 
 app = Flask(__name__)
-Bootstrap(app)
+Bootstrap5(app)
+
+
+@app.route('/brewery_lookup')
+def home(method=['GET']):
+    
+        
+    data = get_single_brewery()
+    
+    return render_template('brewery_lookup.html',data=data)
+
+
+def get_single_brewery():
+    url = 'https://api.openbrewerydb.org/v1/breweries/random'
+    print('Running Data')
+    r = requests.get(url)
+    json_data = json(r)
+    return r.text
+
