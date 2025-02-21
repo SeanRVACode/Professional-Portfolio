3b1ebde5846825bfb98f895e93acbfe1a281ff2f
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2025-02-20 21:19:20 -0500

    Update brewery lookup feature: retrieve multiple breweries and fix HTML template for data display

--- API Website Integration/main.py
+++ API Website Integration/main.py
@@ -12,18 +12,23 @@ Bootstrap5(app)
 def home():
     
         
-    data = get_single_brewery()
-    data_edited = proper_names(data)
-    print(data_edited)
-    
-    return render_template('brewery_lookup.html',data=data_edited)
+    data = get_brewery_list()
+    print(data)
+    print('This is the data type beep boop', type(data[0]))
+    # data_dict = proper_names(data)
+    # print('These are the values',data_dict.values())
+    return render_template('brewery_lookup.html',data=data)
 
 
-def get_single_brewery():
-    url = 'https://api.openbrewerydb.org/v1/breweries/random'
+def get_brewery_list():
+    url = 'https://api.openbrewerydb.org/v1/breweries?per_page=10'
     print('Running Data')
     r = requests.get(url)
+    print(r)
     json_data = r.json()
+    
+    # Convert to dict
+    # brewery_dict = {brewery['id']: brewery for brewery in json_data}
     return json_data
 
 def proper_names(json_data):
