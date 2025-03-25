30a4ace59fbcd5b3daa7d7c9f06ef3e8a0c44bd4
Sean <seanwpgames@gmail.com>
2025-03-25 11:22:39 -0400

    Implement flash messaging for search results and add custom 404 error page

--- API Website Integration/main.py
+++ API Website Integration/main.py
@@ -2,11 +2,15 @@ import re
 from urllib.parse import urlencode
 
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 from flask_bootstrap import Bootstrap5
+from werkzeug.exceptions import HTTPException
+import json
 
 app = Flask(__name__)
+app.secret_key = "testkey"
 Bootstrap5(app)
+
 
 # Default URL for Openbrewerydb
 DEFAULT_URL = "https://api.openbrewerydb.org/v1/breweries"
@@ -23,13 +27,41 @@ def home():
 def search():
     if request.method == "POST":
         json_data = get_brewery_list(request)
-        json_data = create_map_link(json_data)
-        headers_list = proper_names(json_data)
-
-        return render_template(
-            "brewery_lookup.html", data=json_data, headers=headers_list
-        )
+        print(json_data)
+        if json_data and len(json_data) > 0:
+            headers_list = proper_names(json_data)
+            return render_template(
+                "brewery_lookup.html", data=json_data, headers=headers_list
+            )
+        else:
+            print("Attempting Flash Message")
+            # Handle no results case
+            flash("Invalid Search Parameter")
+            return redirect("search_brew")
     return render_template("search.html")
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # Start with the correct headers and status code from the error
+    response = e.get_response()
+    # Replace the body with JSON
+    response.data = json.dumps(
+        {
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        }
+    )
+    response.content_type = "application/json"
+    return response
+
+
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     # note that we set the 404 status explicitly
+#     return render_template("404.html")
 
 
 def get_brewery_list(request=None) -> dict:
@@ -55,8 +87,10 @@ def get_brewery_list(request=None) -> dict:
     r = requests.get(url)
     json_data = r.json()
     json_data = create_map_link(json_data)
-
-    return json_data
+    if json_data:
+        return json_data
+    else:
+        return None
 
 
 def get_random_breweries():
