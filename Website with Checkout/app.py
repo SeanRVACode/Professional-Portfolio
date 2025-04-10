48d1b3ecf94ca406556b3bfd6a3c074a8089d5c1
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2025-04-10 12:43:25 -0400

    Refactor checkout and header templates for improved structure and styling

--- Website with Checkout/app.py
+++ Website with Checkout/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, get_flashed_messages
+from flask import Flask, get_flashed_messages, render_template
 from flask_bootstrap import Bootstrap5
 import os
 from dotenv import load_dotenv
@@ -6,8 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+Bootstrap5(app)
 
 
 @app.route("/")
 def home():
-    return "<p> Hello World!</p>"
+    # FIXME Currently rendering checkout.html for testing purposes
+    return render_template("checkout.html")
