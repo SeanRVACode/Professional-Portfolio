7c235a2d26e475d120ee82797e5204674b8f3a82
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2025-04-11 16:45:30 -0400

    Add product model, database setup, and update store template to display products

--- Website with Checkout/app.py
+++ Website with Checkout/app.py
@@ -4,11 +4,13 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Text
 
 
 load_dotenv()
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "development"
 Bootstrap5(app)
 
 
@@ -18,15 +20,28 @@ class Base(DeclarativeBase):  # TODO Figure out what this does and why
 
 
 # Config App
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///"  # TODO Need to finish this with an actual database?
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"  # TODO Need to finish this with an actual database?
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
 
 # Shop Table
+class Products(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column()
+    amount: Mapped[int] = mapped_column()
+    price: Mapped[float] = mapped_column()
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def home():
     # FIXME Currently rendering checkout.html for testing purposes
-    return render_template("store.html")
+    products = get_products()
+    return render_template("store.html", products=products)
 
 
 @app.route("/checkout")
@@ -35,5 +50,5 @@ def cart():
 
 
 def get_products():
-    # Need to finish this function
-    pass
+    products = db.session.execute(db.select(Products).order_by(Products.name)).scalars()
+    return products
