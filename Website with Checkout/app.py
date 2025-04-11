a6f589bcf63579e4e8836ff3d5ab363ea7c3bb2e
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2025-04-11 14:45:22 -0400

    Implement initial database setup and enhance store layout with product cards and navigation

--- Website with Checkout/app.py
+++ Website with Checkout/app.py
@@ -2,6 +2,9 @@ from flask import Flask, get_flashed_messages, render_template
 from flask_bootstrap import Bootstrap5
 import os
 from dotenv import load_dotenv
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from flask_sqlalchemy import SQLAlchemy
+
 
 load_dotenv()
 
@@ -9,7 +12,28 @@ app = Flask(__name__)
 Bootstrap5(app)
 
 
+# Create Database
+class Base(DeclarativeBase):  # TODO Figure out what this does and why
+    pass
+
+
+# Config App
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///"  # TODO Need to finish this with an actual database?
+
+# Shop Table
+
+
 @app.route("/")
 def home():
     # FIXME Currently rendering checkout.html for testing purposes
+    return render_template("store.html")
+
+
+@app.route("/checkout")
+def cart():
     return render_template("checkout.html")
+
+
+def get_products():
+    # Need to finish this function
+    pass
