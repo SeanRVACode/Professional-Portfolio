0bf4da193b9d785463883094d952a6e2fc541c6f
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2025-04-23 21:18:55 -0400

    Fix formatting in User model: update __repr__ method for consistency and add missing newline

--- Website with Checkout/user_model.py
+++ Website with Checkout/user_model.py
@@ -5,10 +5,11 @@ from app import db
 
 
 class User(db.Model):
+    # These are not instance variables, but class variables with type annotations
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[str] = so.mapped_column(sa.String(256))
 
     def __repr__(self):
-        return f'<User {}'.format(self.username)
+        return f"<User {self.username}"
