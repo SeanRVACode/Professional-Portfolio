b080c76a8ae00b014e6d37f4b61aff95065f7d18
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2025-04-24 12:10:43 -0400

    Integrate user authentication: set up Flask-Login and implement user loader function in the User model

--- Website with Checkout/shop/__init__.py
+++ Website with Checkout/shop/__init__.py
@@ -2,11 +2,17 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap5
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+bootstrap = Bootstrap5(app)
+load_dotenv()
 
 # We import the routes here to avoid circular imports
 from shop import models, routes
--- Website with Checkout/shop/models.py
+++ Website with Checkout/shop/models.py
@@ -5,13 +5,21 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so  # orm stands for Object Relational Mapping. SO stands for SQLAlchemy ORM
 from shop import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from shop import login
+
+
+@login.user_loader
+def load_user(user_id):
+    return db.session.get(User, int(user_id))
+
 
 if TYPE_CHECKING:
     from .payment_method import PaymentMethod  # type: ignore
     from .order import Order  # type: ignore
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     # These are not instance variables, but class variables with type annotations
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
